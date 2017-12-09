668ae9906888cd76279ba0694da182b6fbf1defd
Niggita <wailful@localhost.localdomain>
2017-12-10 00:14:53 +0300

    Some codde here

--- Prototype/Assets/Scripts/WorldObject/Building.cs
+++ Prototype/Assets/Scripts/WorldObject/Building.cs
@@ -4,5 +4,55 @@ using UnityEngine;
 
 public class Building : WorldObject {
 
+	private Renderer objRenderer;
+	private Color mainColor;
+	private bool exit;
+
+	private GameObject[] unitsInside;
+	private int maxUnitsAllowed;
+	private int buildingLevel;
+
+	public bool buildingMenuOpened;
+
+	private void Awake(){
+		base.Awake ();
+		objRenderer = GetComponent<Renderer> ();
+		mainColor = objRenderer.material.color;
+	}
+
+	private void Update(){
+		base.Update ();
+	}
+
+	private void OnMouseEnter(){
+		exit = false;
+	}
+
+	private void OnMouseDown(){
+		StartCoroutine(Blink());
+		buildingMenuOpened = true;
+	}
+
+	private void OnMouseExit(){
+		exit = true;
+		objRenderer.material.color = mainColor;
+	}
+
+	private IEnumerator Blink() {
+		float maxAlpha = 1f;
+		float minAlpha = 0.4f;
+		float alphaDelta = 0.01f;
+		while(true){
+			if (exit)
+				yield break;
+			if (objRenderer.material.color.a <= minAlpha || objRenderer.material.color.a >= maxAlpha)
+				alphaDelta = -alphaDelta;
+			objRenderer.material.SetColor("_Color", new Color(objRenderer.material.color.r,
+				objRenderer.material.color.g,
+				objRenderer.material.color.b,
+				objRenderer.material.color.a + alphaDelta));
+			yield return new WaitForSeconds(0.02f);
+		}
+	}
 
 }
